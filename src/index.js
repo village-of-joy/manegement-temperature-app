981587cc113892142b74b70390378fad5c543a1f
village-of-joy <toshipapa0915.akimama1007@gmail.com>
2025-02-19 22:14:07 +0900

    cognitoログイン認証機能

--- src/index.js
+++ src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import { Amplify } from 'aws-amplify';
+import { AuthProvider } from 'react-oidc-context';
+
+const cognitoAuthConfig = {
+  authority: process.env.REACT_APP_AUTHORITY,
+  client_id: process.env.REACT_APP_CLIENT_ID,
+  redirect_uri: process.env.REACT_APP_REDIRECT_URL,
+  response_type: "code",
+  scope: "email openid phone",
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider {...cognitoAuthConfig}>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
 
